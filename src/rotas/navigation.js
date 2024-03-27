ef13fd9ad51b9605250f5e544db7b1982b97f737
Guilherme Livianu <guilherme.livianu@gmail.com>
2024-03-27 11:25:26 -0300

    Tab Navigation

--- src/rotas/navigation.js
+++ src/rotas/navigation.js
@@ -1,10 +1,24 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
 import Home from "../paginas/Home";
 import Login from "../paginas/Login";
 import Cadastro from "../paginas/Cadastro";
+import ListaPets from "../paginas/ListaPets";
+import Mensagem from "../paginas/Mensagem";
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function TabRoutes() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="ListaPets" component={ListaPets} />
+      <Tab.Screen name="Mensagem" component={Mensagem} />
+    </Tab.Navigator>
+  );
+}
 
 export default function Navigation() {
   return (
