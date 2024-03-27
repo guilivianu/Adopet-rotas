fdc53beee7681fda3cf4c23aee76779a862366a5
Guilherme Livianu <guilherme.livianu@gmail.com>
2024-03-27 11:49:31 -0300

    Customização de abas

--- src/rotas/navigation.js
+++ src/rotas/navigation.js
@@ -7,15 +7,38 @@ import Login from "../paginas/Login";
 import Cadastro from "../paginas/Cadastro";
 import ListaPets from "../paginas/ListaPets";
 import Mensagem from "../paginas/Mensagem";
+import { Image } from "react-native";
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 function TabRoutes() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="ListaPets" component={ListaPets} />
-      <Tab.Screen name="Mensagem" component={Mensagem} />
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Screen
+        name="Lista de Pets"
+        component={ListaPets}
+        options={{
+          tabBarIcon: () => (
+            <Image
+              source={require("../assets/pets-green.png")}
+              style={{ width: 24, height: 24 }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Mensagem"
+        component={Mensagem}
+        options={{
+          tabBarIcon: () => (
+            <Image
+              source={require("../assets/mensagens.png")}
+              style={{ width: 24, height: 24 }}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
